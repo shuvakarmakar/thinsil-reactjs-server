8333e4f96dfad1d4a536f586b2b1c3c9f9c88cd6
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-10-25 20:49:03 +0600

    Fix some Error

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const usersCollection = client.db('thinsilDB').collection('users');
         const productsCollection = client.db('thinsilDB').collection('products');
